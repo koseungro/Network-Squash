1954eddda44219723177c3eb9a9050353f23536a
kosengro <goseungro@naver.com>
2019-10-15 15:54:52 +0900

    10/15

--- Network Squash/Assets/02.Scripts/Racket.cs
+++ Network Squash/Assets/02.Scripts/Racket.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class Racket : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Vector3 _velocity;
 
-    private float _sensitivity = 100f;
+    private Vector3 curRacket;
+    private Vector3 preRacket;
+    private Vector3 racketVel;
+    private Vector3 ballHit;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        // ballHit = BallCtrl.instance.ballVel;
     }
 
-    
+
     void Update()
     {
-        
+        RacketSwing();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.transform.CompareTag("BALL"))
+        {           
+            float power = racketVel.magnitude;
+            Debug.Log((power));
+            // Vector3 dir = other.Vector3
+            other.GetComponent<Rigidbody>().velocity = racketVel + BallCtrl.instance.ballPower*5f;
+        }
+    }
+    void RacketSwing()
+    {
+        curRacket = transform.position;
+        racketVel = (curRacket - preRacket) / Time.deltaTime;
+        // float power = racketVel.magnitude;
+
+        preRacket = curRacket;
     }
 }
