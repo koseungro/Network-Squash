969845f1771b57cf398c954ae938aae64b4b737b
VRtistHan <54020605+VRtistHan@users.noreply.github.com>
2019-10-15 11:38:41 +0900

    working on bounce

--- Network Squash/Assets/02.Scripts/TestRacket.cs
+++ Network Squash/Assets/02.Scripts/TestRacket.cs
@@ -25,5 +25,6 @@ public class TestRacket : MonoBehaviour
             transform.position - target.transform.position);
         Vector3 bounceVector =
             Vector3.Reflect(coll.relativeVelocity, inNormal);
+            target.GetComponent<Rigidbody>().AddForce(bounceVector * force, ForceMode.VelocityChange);
     }
 }
