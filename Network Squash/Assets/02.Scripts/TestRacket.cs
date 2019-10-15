4b66e1d1bd15ed0c17f3c7bc0432e894df1b274e
choiwansu <wansu7007@naver.com>
2019-10-15 11:15:28 +0900

    Add Racket Coll

---
+++ Network Squash/Assets/02.Scripts/TestRacket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRacket : MonoBehaviour
+{
+    public float force = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject target = coll.gameObject;
+
+        Vector3 inNormal = Vector3.Normalize(
+            transform.position - target.transform.position);
+        Vector3 bounceVector =
+            Vector3.Reflect(coll.relativeVelocity, inNormal);
+    }
+}
