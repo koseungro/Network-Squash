b250a8b48c993c9cc2c1593add7911e68a211b02
VRtistHan <54020605+VRtistHan@users.noreply.github.com>
2019-10-16 13:31:54 +0900

    working on network

---
+++ Network Squash/Assets/02.Scripts/NetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class NetworkManager : Photon.MonoBehaviour
+{
+	public Transform spawnPoint1;
+	public Transform spawnPoint2;
+
+
+
+	void Start()
+	{
+		photonView.RPC("StartGame", PhotonTargets.All);
+	}
+
+	[PunRPC]
+	void StartGame()
+	{
+		
+		Debug.Log("Joined Room!");
+		PhotonNetwork.Instantiate("SquashPlayer", spawnPoint1.position, spawnPoint1.rotation, 0);
+	}
+}
