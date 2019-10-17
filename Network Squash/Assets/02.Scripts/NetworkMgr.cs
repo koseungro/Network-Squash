7b00b680415e006a2fffeb941a80c5d346580cdd
VRtistHan <54020605+VRtistHan@users.noreply.github.com>
2019-10-17 18:22:15 +0900

    instantiate racket

--- Network Squash/Assets/02.Scripts/NetworkMgr.cs
+++ Network Squash/Assets/02.Scripts/NetworkMgr.cs
@@ -7,11 +7,17 @@ public class NetworkMgr : Photon.MonoBehaviour
 	public Transform player1Pos;
 	public Transform ballSpawnPoint;
 
+  public Transform racketSpawnPoint1;
+
+  public Transform racketSpawnPoint2;
+
     // Start is called before the first frame update
     void Start()
     {
 		PhotonNetwork.Instantiate("NetworkPlayer", player1Pos.position, player1Pos.rotation, 0);
 		PhotonNetwork.InstantiateSceneObject("Ball_Network", ballSpawnPoint.position, ballSpawnPoint.rotation, 0, null);
+    PhotonNetwork.InstantiateSceneObject("Racket_Network", racketSpawnPoint1.position, racketSpawnPoint1.rotation, 0, null);
+    PhotonNetwork.InstantiateSceneObject("Racket_Network", racketSpawnPoint2.position, racketSpawnPoint2.rotation, 0, null);
     }
 
     // Update is called once per frame
