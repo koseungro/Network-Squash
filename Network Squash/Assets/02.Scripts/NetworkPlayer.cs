d2cea19b22dd0faffa717f8ff75df86ac19d1fa7
VRtistHan <54020605+VRtistHan@users.noreply.github.com>
2019-10-20 18:36:03 +0900

    not syncing hand motion with other player

--- Network Squash/Assets/02.Scripts/NetworkPlayer.cs
+++ Network Squash/Assets/02.Scripts/NetworkPlayer.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Valve.VR;
 public class NetworkPlayer : Photon.MonoBehaviour
 {
 	public GameObject playerHead;
 	Camera cam;
 	AudioListener sound;
-    
+
+	public GameObject lController;
+	public GameObject rController;
+
+	private SteamVR_Behaviour_Pose lHandBehavior;
+	private SteamVR_Behaviour_Pose rHandBehavior;
 
 	// Start is called before the first frame update
 	void Start()
@@ -19,7 +24,15 @@ public class NetworkPlayer : Photon.MonoBehaviour
 
 			sound = playerHead.GetComponent<AudioListener>();
 			sound.enabled = true;
+
 			// myCamera.SetActive(true);
+			//내 손 움직임만 허용
+			lHandBehavior = lController.GetComponent<SteamVR_Behaviour_Pose>();
+			rHandBehavior = rController.GetComponent<SteamVR_Behaviour_Pose>();
+			lHandBehavior.enabled = true;
+			rHandBehavior.enabled = true;
+
+
 		}
     }
 
